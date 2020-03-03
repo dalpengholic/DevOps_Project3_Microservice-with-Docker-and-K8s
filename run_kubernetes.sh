4f19f5dd411f75a23b91d98ff86591dd10f7463c
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-03-03 14:44:18 +0200

    first commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=dalpengholic/UdacityProject4:latest
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run UdacityProject4 --port=80 --image=$dockerpath
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment/UdacityProject4 8000:80
+
+# Step 5:
+# Show logs
+kubectl logs `kubectl get pods -o-name` 
