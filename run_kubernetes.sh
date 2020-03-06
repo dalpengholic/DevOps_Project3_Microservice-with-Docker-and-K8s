ecbbab0176a37f39472ba30280cf8335177aa803
Gabriel Woojae Lim <dalpengholic@gmail.com>
2020-03-06 15:46:08 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=dalpengholic/UdacityProject4:latest
+dockerpath=dalpengholic/udacityproject4:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run UdacityProject4 --port=80 --image=$dockerpath
+kubectl run udacityproject4 --port=80 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
@@ -17,7 +17,7 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/UdacityProject4 8000:80
+kubectl port-forward deployment/udacityproject4 8000:80
 
 # Step 5:
 # Show logs
